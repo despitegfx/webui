e2a4f51e4995a8a8c519f6f07b9efdd16342fe9f
Emmanuel Bonsu <despitegfx@Emmanuels-MacBook-Pro.local>
2021-03-06 14:13:31 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent},
+  { path: 'body', component: BodyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
